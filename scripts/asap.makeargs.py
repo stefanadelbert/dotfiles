7f15d729b3ee97e42baad429dcbe4ffd7bf8c85d
Stefan Adelbert <stefan.adelbert@gmail.com>
2015-02-10 12:27:41 +1100

    Added out-of-tree/in-tree building to asap.makeargs.py; Added a snippet for cpp;

--- scripts/asap.makeargs.py
+++ scripts/asap.makeargs.py
@@ -7,16 +7,28 @@ def main(argv):
         assert os.path.basename(os.getcwd()) == 'ASAP', 'Invalid current directory'
         sys.exit(0)
 
+    out_of_tree = False
+
     assert len(argv) == 1, "Can't have more than 1 parameter"
 
     if os.path.isfile(argv[0]):
         abspath = os.path.abspath(argv[0])
-        buildpath = os.path.dirname(abspath).replace("/ASAP/", "/.build/")
+
+        if out_of_tree:
+            buildpath = os.path.dirname(abspath).replace("/ASAP/", "/.build/")
+        else:
+            buildpath = os.path.dirname(abspath)
+
         filename = os.path.basename(abspath)
         print "-C {} {}.o".format(buildpath, filename)
     elif os.path.isdir(argv[0]):
         abspath = os.path.abspath(argv[0])
-        buildpath = abspath.replace("/ASAP/", "/.build/")
+
+        if out_of_tree:
+            buildpath = abspath.replace("/ASAP/", "/.build/")
+        else:
+            buildpath = abspath
+
         print "-C {}".format(buildpath)
 
 if __name__ == "__main__":
